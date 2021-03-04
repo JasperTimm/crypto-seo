92d6682ae9a0b67e55af22b6ff6bb5b8b4d55908
Jasper Timm <jasper.timm@ehealthafrica.org>
2021-03-04 19:11:33 +0000

    Why won't truffle run...

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,6 @@ serverless deploy
 export SEARCH_URL=`serverless info | grep 'GET -' | sed 's/GET -//g' | xargs`
 cd ..
 
-truffle migrate --network kovaninfura
+npx truffle migrate --network kovaninfura
 npm run build-frontend
 rsync -r --delete-after $TRAVIS_BUILD_DIR/dist travis-ci@jaspa.codes:/var/www/crypto-seo
